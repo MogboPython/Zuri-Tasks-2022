91243ac0d20deb7454d41e4a0768d07c72b1963c
Mogboluwaga Onayade <mogbo18@yahoo.com>
2022-06-25 12:17:22 +0100

    New Task Django CRUD submission

---
+++ I4G009145TQZ_DjangoCrud/blog/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.views.generic import ListView, CreateView, DetailView, DeleteView, UpdateView
+from .models import Post
+from django.urls import reverse_lazy
+
+class PostListView(ListView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDeleteView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+
+
